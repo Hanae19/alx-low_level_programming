705e9f42bb625c2df7feed054408568081151b0e
Hanae19 <hasnaeyassine1977@gmail.com>
2023-02-20 09:04:20 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-**
-*main  Entry point
+/**
+*main - main function
 *
 *Return: 0 (Success)
-*\
-
+*/
 int main(void)
 {
 int s = 97, e = 122;
@@ -17,11 +16,13 @@ s++;
 
 s = 65;
 e = 90;
+
 while (s <= e)
 {
 putchar(s);
 s++;
 }
+
 putchar('\n');
 
 return (0);
